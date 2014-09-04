b5db56c26bdde3013ea2545cc7e45a8b7ac7c5a6
Kondal Rao Komaragiri <kondal04@gmail.com>
2014-09-04 14:57:07 -0700

    Initial Commit

---
+++ SwiftMacApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftMacApp
+//
+//  Created by Kondal Rao Komaragiri on 9/4/14.
+//  Copyright (c) 2014 Kondal Rao Komaragiri. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwiftMacApp/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SwiftMacApp
+//
+//  Created by Kondal Rao Komaragiri on 9/4/14.
+//  Copyright (c) 2014 Kondal Rao Komaragiri. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
